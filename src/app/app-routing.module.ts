ce646e3f7c5b6d766725b68a77057e889b6ac76a
Sunil Sarode <sunil.sarode@xpo.com>
2020-11-02 15:55:52 +0530

    Initial work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CustomersModule } from './customers/customers.module';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: 'customers',
+    loadChildren: () => CustomersModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
